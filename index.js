da58f6835070318ef8f14ac7e25edc42f0b191be
karimzz701 <karimzz701@gmail.com>
2024-06-27 13:08:57 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const app = require("express")()  ;
 
 const port = 8080 ; 
 
-app.listen( port, ()=>{
+app.listen( port || process.env.PORT, ()=>{
     console.log(`Server running on Port ${port}`)
 })
 
